466f3fdae12656bbfb800d17a5e2a98b545f265f
mihirpatel1510 <mihir.patel@team.satat.tech>
2024-10-04 11:45:59 +0530

    complete 6 sections

--- src/server.js
+++ src/server.js
@@ -1,24 +1,23 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
 
-app.use(express.static(path.join(__dirname,'public')));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res ,next)=> {
-    res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-
 app.listen(3000);
-
-// module.exports = path.dirname(require.main.filename);
